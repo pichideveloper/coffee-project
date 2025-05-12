ff6568f667e335a3d7797582a4873c615fd41e8b
Kenzowo-dev <kenzoquiros@icloud.com>
2025-05-11 20:20:46 -0500

    g

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
